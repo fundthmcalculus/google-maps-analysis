e43399cb4088972738314d02fe0cb3437b77c1d9
Scott Phillips <scott.phillips@earnin.com>
2021-04-21 19:54:10 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+import argparse
+import logging
+import os
+import sys
+from typing import Dict, Callable
+
+import requests as requests
+
+import kmlutilities
+
+
+def create_function_map() -> Dict[str, Callable]:
+    return {
+            'traillength': trail_length
+            }
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', help="Perform an action", choices=create_function_map().keys())
+    parser.add_argument('--reportfile', help="Output csv file")
+    parser.add_argument('--summarycolumns', help="Summary columns for report, separated by commas `,`")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--kmlfile', help="Get the KML file")
+    group.add_argument('--mapurl', help="Map URL")
+    return parser.parse_args()
+
+
+def trail_length() -> None:
+    args = parse_arguments()
+    summary_columns = args.summarycolumns.split(',')
+
+    if args.mapurl:
+        r = requests.get(args.mapurl, allow_redirects=True)
+        open('kml_file.kml', 'wb').write(r.content)
+        args.kmlfile = 'kml_file.kml'
+
+    logging.info(f"Parsing KML file={args.kmlfile}, summary columns={summary_columns}")
+    doc = kmlutilities.parse_file(args.kmlfile)
+    trail_data, report_data, trails_report = kmlutilities.process_kml_data(doc)
+    kmlutilities.create_pivot_table_summaries(trail_data, summary_columns, args.reportfile)
+    kmlutilities.write_report_files(trail_data, report_data, trails_report, args.reportfile)
+
+
+def main():
+    dir_path: str = os.path.dirname(os.path.realpath(__file__))
+    log_file = os.path.join(dir_path, 'maps-analysis.log')
+    logging.basicConfig(filename=log_file,
+                        format='%(asctime)s:%(levelname)s:%(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        filemode='w',
+                        level=logging.DEBUG)
+    logging.debug('Started')
+    try:
+        args = parse_arguments()
+        func = create_function_map()[args.command]
+        func()
+        logging.debug('Finished')
+
+        sys.exit(0)
+    except Exception as err:
+        logging.exception('Fatal error in main:', exc_info=True)
+        sys.exit(-1)
+
+
+if __name__ == '__main__':
+    main()
